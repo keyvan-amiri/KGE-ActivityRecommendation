46b3ba0245200a2cc7a4451b3ed90df098020caf
keyvan-amiri <kamiriel@uni-mannheim.de>
2023-11-22 15:42:41 +0100

    Added python file for transfers

---
+++ file_transfer.py
@@ -0,0 +1,45 @@
+import os
+import shutil
+
+def copy_files_with_patterns(source_folders, target_folders, file_patterns):
+    for source, target in zip(source_folders, target_folders):
+        # Ensure source and target folders exist
+        if not os.path.exists(source):
+            print(f"Source folder '{source}' does not exist. Skipping.")
+            continue
+        if not os.path.exists(target):
+            os.makedirs(target)
+
+        # Get a list of files in the source folder that match the specified patterns
+        files_to_copy = [file for file in os.listdir(source) if any(pattern in file for pattern in file_patterns)]
+
+        # Copy each matching file to the target folder
+        for file_name in files_to_copy:
+            source_path = os.path.join(source, file_name)
+            target_path = os.path.join(target, file_name)
+            shutil.copy2(source_path, target_path)
+            print(f"Copied '{file_name}' from '{source}' to '{target}'.")
+
+if __name__ == "__main__":
+    # Get the path to the directory containing the script
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    # Construct path to the bpmai datasets folder
+    bpmai_path = os.path.join(script_dir, "bpmai datasets")
+    # Construct the path to the "data" folder
+    local_folder_path = os.path.join(script_dir, "..")
+    kge_folder_path = os.path.join(local_folder_path, "..")
+    data_folder_path = os.path.join(kge_folder_path, "data")
+    # define dataset names
+    dataset_names = ["bpmai_lastrev_caise_onlyAfter", "bpmai_lastrev_caise_onlyAfter2",
+                     "bpmai_lastrev_caise_inProcess", "bpmai_lastrev_caise_inProcess2",
+                     "bpmai_lastrev_caise", "bpmai_lastrev_caise2"]
+    # Specify the file patterns for copy
+    file_patterns = [".txt", ".yml"]
+    
+    # define source and target folders 
+    source_folders, target_folders = [], []
+    for i in range (6):
+        source_folders.append(os.path.join(bpmai_path, dataset_names[i]))
+        target_folders.append(os.path.join(data_folder_path, dataset_names[i]))
+
+    copy_files_with_patterns(source_folders, target_folders, file_patterns)
