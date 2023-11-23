d9d250db1e05e5c52ba490eab8a7b851dcd66822
keyvan-amiri <kamiriel@uni-mannheim.de>
2023-11-23 12:14:17 +0100

    edit transfer file

--- file_transfer.py
+++ file_transfer.py
@@ -18,13 +18,14 @@ def copy_files_with_patterns(source_folders, target_folders, file_patterns):
             source_path = os.path.join(source, file_name)
             target_path = os.path.join(target, file_name)
             shutil.copy2(source_path, target_path)
-            print(f"Copied '{file_name}'.")
+            #print(f"Copied '{file_name}'.")
 
 if __name__ == "__main__":
     # Get the path to the directory containing the script
     script_dir = os.path.dirname(os.path.abspath(__file__))
     # Construct path to the bpmai datasets folder
     bpmai_path = os.path.join(script_dir, "bpmai datasets")
+    configs_path = os.path.join(script_dir, "configs")
     # Construct the path to the "data" folder
     local_folder_path = os.path.join(script_dir, "..")
     kge_folder_path = os.path.join(local_folder_path, "..")
@@ -37,7 +38,7 @@ if __name__ == "__main__":
     # Specify the file patterns for copy
     file_patterns = [".txt", ".yml"]
     
-    # define source and target folders 
+    # Transfer all dataset files 
     source_folders, target_folders = [], []
     for i in range (6):
         source_folders.append(os.path.join(bpmai_path, dataset_names[i]))
@@ -45,9 +46,17 @@ if __name__ == "__main__":
 
     copy_files_with_patterns(source_folders, target_folders, file_patterns)
     
-    # cope default preprocess file to handle preprocessing of datasets smoothly
+    # Transfer all configuration files 
+    source_folders, target_folders = [], []
+    for i in range (6):
+        source_folders.append(os.path.join(configs_path, dataset_names[i]))
+        target_folders.append(os.path.join(data_folder_path, dataset_names[i]))
+
+    copy_files_with_patterns(source_folders, target_folders, file_patterns)
+    
+    # copy default preprocess file to handle preprocessing of datasets smoothly
     preprocess_file = "preprocess_default.py"
     preprocess_source_path = os.path.join(preprocess_folder_path, preprocess_file)
     preprocess_destination_path = os.path.join(data_folder_path, preprocess_file)
     shutil.copy(preprocess_source_path, preprocess_destination_path)
-    print(f"File copied from {preprocess_source_path} to {preprocess_destination_path}")
+    #print(f"File copied from {preprocess_source_path} to {preprocess_destination_path}")
